f24331a3d484eea079b353a79c64d874b5c3f6d3
dejourford <dejourford@my.unt.edu>
2023-07-13 12:00:16 -0500

    create open and close function for mobile nav menu

---
+++ script.js
@@ -0,0 +1,10 @@
+// open and close nav menu
+const mobileNav = document.querySelector('.mobile-nav')
+function closeMenu() {
+    mobileNav.classList.toggle('active')
+}
+const closeBtn = document.querySelector('.close-btn').addEventListener('click', function() {
+    console.log('clicked!')
+    
+})
+

60f1e26dfc593af25e4af88c8ca95a0072e4a701
dejourford <dejourford@my.unt.edu>
2023-07-14 00:00:57 -0500

    add functions for image hover effects

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ function openMenu() {
 const openBtn = document.querySelector('.open-btn').addEventListener('click', function() {
     console.log('clicked')
 })
+
+
+
+// logo image change on mouseenter
+const selfieImage = document.querySelector('.selfie')
+function changeImage() {
+    selfieImage.src = 'images/wavingmoji.webp'
+}
+
+// logo image change on mouseleave
+function changeImageBack() {
+    selfieImage.src = 'images/selfiemoji.webp'
+}

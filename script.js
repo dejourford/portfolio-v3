6d75f6279361253f9b95e52065844fd862067e00
dejourford <dejourford@my.unt.edu>
2023-07-15 01:18:22 -0500

    deleted unused functions

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 // open navigation menu
 const mobileNav = document.querySelector('.mobile-nav')
 function closeMenu() {
-    mobileNav.classList.toggle('active')
+  mobileNav.classList.toggle('active')  
+  
 }
-const closeBtn = document.querySelector('.close-btn').addEventListener('click', function() {
-    console.log('clicked!')
-    
-})
+
 
 // close navigation menu
 function openMenu() {
-    mobileNav.classList.toggle('active')
+    
+  mobileNav.classList.toggle('active')
 }
-const openBtn = document.querySelector('.open-btn').addEventListener('click', function() {
-    console.log('clicked')
-})
+
 
 
 
@@ -29,7 +26,7 @@ function changeImageBack() {
     selfieImage.src = 'images/selfiemoji.webp'
 }
 
-// marquee scroll
+// work marquee scroll
 function Marquee(selector, speed) {
     const parentSelector = document.querySelector(selector);
     const clone = parentSelector.innerHTML;
@@ -51,4 +48,28 @@ function Marquee(selector, speed) {
   //after window is completed load
   //1 class selector for marquee
   //2 marquee speed 0.2
-  window.addEventListener('load', Marquee('.marquee', 0.1))
+  window.addEventListener('load', Marquee('.work-marquee', 0.1))
+
+  // contact marquee scroll
+function Marquee(selector, speed) {
+    const parentSelector = document.querySelector(selector);
+    const clone = parentSelector.innerHTML;
+    const firstElement = parentSelector.children[0];
+    let i = 0;
+    console.log(firstElement);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+  
+    setInterval(function () {
+      firstElement.style.marginLeft = `-${i}px`;
+      if (i > firstElement.clientWidth) {
+        i = 0;
+      }
+      i = i + speed;
+    }, 0);
+  }
+  
+  //after window is completed load
+  //1 class selector for marquee
+  //2 marquee speed 0.2
+  window.addEventListener('load', Marquee('.contact-marquee', 0.1))
